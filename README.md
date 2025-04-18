febebf19d8d91278036d8eb54b62c7eb56148d98
shubhamchaurasiya12 <xshubhamchaurasiya@gmail.com>
2025-04-18 21:56:20 +0530

    This is my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Myntra review scraper project
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Scraper',
+    version='0.0.1',
+    author='Shubham Chaurasiya',
+    author_email='xshubhamchaurasiya@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
